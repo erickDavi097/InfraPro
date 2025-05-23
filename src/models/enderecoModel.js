0e55d022721b13123c1ed2688f8bbfe34c573463
Erickdavi097 <erick.silva097@sptech.school>
2025-05-23 05:01:03 -0300

    ajuste básico no cadastro (um console.log estava escrito errado) e botão que direciona os profissionais a realizarem uma nova publicação caso desejarem.

--- src/models/enderecoModel.js
+++ src/models/enderecoModel.js
@@ -21,7 +21,7 @@ function buscarEnderecosPorUsuario(usuarioId) {
 function cadastrar(cep, uf, cidade, bairro) {
     console.log("ACESSEI O ENDEREÇO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", cep, uf, cidade, bairro);
     const instrucaoSql = `INSERT INTO endereco (cep, uf, cidade, bairro) VALUES ('${cep}', '${uf}', '${cidade}', '${bairro}');`;
-    co.Promisensole.log("Executando a instrução SQL: \n" + instrucaoSql);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 function buscarPorUsuarioId(idUsuario) {
